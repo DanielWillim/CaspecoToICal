f2c2dac6f8d316e9a4c0b466ac84ec88ef935f92
DanielWillim <daniel@willim.se>
2022-08-15 14:17:56 +0200

    Small improvements

--- readPDFtoIcal.py
+++ readPDFtoIcal.py
@@ -1,4 +1,5 @@
-import tabula
+from pprint import pprint
+from tabula import read_pdf
 import pandas
 import numpy as np
 import icalendar as ical
@@ -6,28 +7,33 @@ from icalendar import Calendar, Event
 from datetime import datetime
 from dateutil import parser
 
-#read file
-file = "data.pdf"
-df = tabula.read_pdf(file, pages='all')
+def caspecoToCal(inPath, outPath):
+    #read file
 
-data = df[0].values
+    df = read_pdf(inPath, pages='all')
 
-for i in range(1, len(df)):
-    data = np.concatenate((data, [df[i].columns.tolist()], df[i].values), axis=0)
+    data = df[0].values
 
-data = data[:-1]
+    for i in range(1, len(df)):
+        data = np.concatenate((data, [df[i].columns.tolist()], df[i].values), axis=0)
 
-#Create calendar
-cal = Calendar()
+    data = data[:-1]
 
-for curr in data:
-    event = Event()
-    event.add('summary', curr[0] + " - " + curr[1])
-    event.add('dtstart', parser.parse(curr[2]))
-    event.add('dtend', parser.parse(curr[3]))
+    #Create calendar
+    cal = Calendar()
 
-    cal.add_component(event)
+    for curr in data:
+        event = Event()
+        event.add('summary', curr[0] + " - " + curr[1])
+        event.add('dtstart', parser.parse(curr[2]))
+        event.add('dtend', parser.parse(curr[3]))
 
-f = open('Output/harryda.ics', 'wb')
-f.write(cal.to_ical())
-f.close()
+        cal.add_component(event)
+
+    f = open(outPath, 'wb')
+    f.write(cal.to_ical())
+    f.close()
+
+caspecoToCal("summer2022/Bokningsläge Härryda.pdf", "Output/härryda.ics")
+caspecoToCal("summer2022/Bokningsläge JV.pdf", "Output/jvg.ics")
+caspecoToCal("summer2022/Bokningsläge KG.pdf", "Output/kg.ics")
